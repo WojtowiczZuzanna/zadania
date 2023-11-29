776b4b7faef33bf8a9e641c5074dc321700f745a
WojtowiczZuzanna <146974708+WojtowiczZuzanna@users.noreply.github.com>
2023-11-29 18:13:40 +0100

    during class 08

---
+++ 08/08_08.py
@@ -0,0 +1,11 @@
+mobile = {
+    "OS":"Android",
+    "brand":"Samsung",
+    "production":"South Korea",
+    "model":"Galaxy ...",
+    "year":2020,
+    "color":"black"
+}
+
+for key,value in mobile.items():
+    print(f"{key} : {value}")
